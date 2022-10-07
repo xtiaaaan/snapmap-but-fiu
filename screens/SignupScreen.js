4f4107ce310f267c6eb0324dcec969051c406a35
xtiaaaan <nico.angus3@gmail.com>
2022-10-07 17:33:29 -0400

    Created Cancel Button that sends user back to Home Screen

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,10 +1,14 @@
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const SignupScreen = ({ navigation }) => {
   return (
     <SafeAreaView>
       <Text>SignupScreen</Text>
+      <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+        <Text>Cancel</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
